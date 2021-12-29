3405e9108e726a55e6adf97d0db35b6171db40a1
Diego Magaldi <realiversity@hotmail.com>
2021-12-28 23:43:24 -0600

    active vuetify

--- harvardartmuseums/src/App.vue
+++ harvardartmuseums/src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <v-app>
+        <navigation :color="color" :flat="flat" />
+        <v-main class="pt-0">
+        </v-main>
+        <v-scale-transition>
+        <v-btn
+            fab
+            v-show="fab"
+            v-scroll="onScroll"
+            dark
+            fixed
+            bottom
+            right
+            color="secondary"
+            @click="toTop"
+        >
+            <v-icon>mdi-arrow-up</v-icon>
+        </v-btn>
+        </v-scale-transition>
+        <foote />
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import navigation from "./components/Navigation";
+import foote from "./components/Footer";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+    name: "App",
+    components: {
+        navigation,
+        foote
+    },
+    data: () => ({
+        fab: null,
+        color: "",
+        flat: null,
+    }),
+
+    created() {
+        const top = window.pageYOffset || 0;
+        if (top <= 60) {
+        this.color = "transparent";
+        this.flat = true;
+        }
+    },
+
+    watch: {
+        fab(value) {
+        if (value) {
+            this.color = "secondary";
+            this.flat = false;
+        } else {
+            this.color = "transparent";
+            this.flat = true;
+        }
+        },
+    },
+
+    methods: {
+        onScroll(e) {
+        if (typeof window === "undefined") return;
+        const top = window.pageYOffset || e.target.scrollTop || 0;
+        this.fab = top > 60;
+        },
+        toTop() {
+        this.$vuetify.goTo(0);
+        },
+    },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.v-main {
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
 }
 </style>
--- harvardartmuseums/src/main.js
+++ harvardartmuseums/src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
+//const key = '1df4449c-4dd6-4d40-8afd-7aa6d735f656'
 new Vue({
-  render: (h) => h(App),
+    vuetify,
+    render: (h) => h(App),
 }).$mount("#app");
